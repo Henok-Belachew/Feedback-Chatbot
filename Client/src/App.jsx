1951e3e8b014207ab5fd991a0a73e6bd4dabe98d
Henok Belachew <henokbelachewstu@gmail.com>
2023-08-10 10:13:19 +0300

    ChatApp file is added

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import ChatApp from './ChatApp'
 
 function App() {
   return (
     <div className='text-red-700'>
       Your react is working fine!
+      <ChatApp/>
     </div>
   )
 }
---
+++ Client/src/ChatApp.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+function ChatApp() {
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState('');
+
+  const handleSendMessage = () => {
+    if (newMessage.trim() === '') return;
+
+    const newMessages = [...messages, { text: newMessage, user: 'user' }];
+    setMessages(newMessages);
+    setNewMessage('');
+    // Add logic to send the message to the chatbot backend here
+  };
+
+  return (
+    <div className="chat-container">
+      <div className="chat-messages">
+        {messages.map((message, index) => (
+          <div key={index} className={`message ${message.user}`}>
+            {message.text}
+          </div>
+        ))}
+      </div>
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="Type your message..."
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+          onKeyPress={(e) => {
+            if (e.key === 'Enter') {
+              handleSendMessage();
+            }
+          }}
+        />
+        <button onClick={handleSendMessage}>Send</button>
+      </div>
+    </div>
+  );
+}
+
+export default ChatApp;
